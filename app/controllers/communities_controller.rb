13cdcc81156931cd7f2186bd8a507c7647871271
ko-suke24 <kosuke0322@gmail.com>
2020-11-18 12:50:41 +0900

    community base

---
+++ app/controllers/communities_controller.rb
@@ -0,0 +1,16 @@
+class CommunitiesController < ApplicationController
+  before_authentication :authenticate_account!, except: [ :index, :show ]
+
+  def index
+    @communities = Community.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
